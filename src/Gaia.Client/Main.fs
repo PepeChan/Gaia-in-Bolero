4cf897f0e494bdeaaaa8ad2d59ae4edff859c7f6
Pepe Vera <your-github-email@example.com>
2026-05-31 07:04:51 +0200

    removed original bolero template book store example

--- src/Gaia.Client/Main.fs
+++ src/Gaia.Client/Main.fs
@@ -11,31 +11,15 @@ open Gaia.Core
 
 /// Routing endpoints definition.
 type Page =
-    | [<EndPoint "/">] Home
-    | [<EndPoint "/counter">] Counter
-    | [<EndPoint "/data">] Data
+    | [<EndPoint "/">] Probe
 
 /// The Elmish application's model.
 type Model =
     {
         page: Page
-        counter: int
-        books: Book[] option
         error: string option
-        username: string
-        password: string
-        signedInAs: option<string>
-        signInFailed: bool
         selectedScenarioId: string option
         scenarioResolution: ResolutionView option
-    }
-
-and Book =
-    {
-        title: string
-        author: string
-        publishDate: DateTime
-        isbn: string
     }
 
 let demoScenarios = DemoData.demoScenarios
@@ -62,105 +46,23 @@ let initModel =
         |> Option.bind resolveScenario
 
     {
-        page = Home
-        counter = 0
-        books = None
+        page = Probe
         error = None
-        username = ""
-        password = ""
-        signedInAs = None
-        signInFailed = false
         selectedScenarioId = selectedScenarioId
         scenarioResolution = scenarioResolution
     }
-
-/// Remote service definition.
-type BookService =
-    {
-        /// Get the list of all books in the collection.
-        getBooks: unit -> Async<Book[]>
-
-        /// Add a book in the collection.
-        addBook: Book -> Async<unit>
-
-        /// Remove a book from the collection, identified by its ISBN.
-        removeBookByIsbn: string -> Async<unit>
-
-        /// Sign into the application.
-        signIn : string * string -> Async<option<string>>
-
-        /// Get the user's name, or None if they are not authenticated.
-        getUsername : unit -> Async<string>
-
-        /// Sign out from the application.
-        signOut : unit -> Async<unit>
-    }
-
-    interface IRemoteService with
-        member this.BasePath = "/books"
-
 /// The Elmish application's update messages.
 type Message =
     | SetPage of Page
-    | Increment
-    | Decrement
-    | SetCounter of int
-    | GetBooks
-    | GotBooks of Book[]
-    | SetUsername of string
-    | SetPassword of string
-    | ClearLoginForm
-    | GetSignedInAs
-    | RecvSignedInAs of option<string>
-    | SendSignIn
-    | RecvSignIn of option<string>
-    | SendSignOut
-    | RecvSignOut
     | SelectScenario of string
     | Error of exn
     | ClearError
 
-let update remote message model =
-    let onSignIn = function
-        | Some _ -> Cmd.batch [ Cmd.ofMsg GetBooks; Cmd.ofMsg ClearLoginForm ]
-        | None -> Cmd.none
+let update message model =
     match message with
     | SetPage page ->
         { model with page = page }, Cmd.none
 
-    | Increment ->
-        { model with counter = model.counter + 1 }, Cmd.none
-    | Decrement ->
-        { model with counter = model.counter - 1 }, Cmd.none
-    | SetCounter value ->
-        { model with counter = value }, Cmd.none
-
-    | GetBooks ->
-        let cmd = Cmd.OfAsync.either remote.getBooks () GotBooks Error
-        { model with books = None }, cmd
-    | GotBooks books ->
-        { model with books = Some books }, Cmd.none
-
-    | SetUsername s ->
-        { model with username = s }, Cmd.none
-    | SetPassword s ->
-        { model with password = s }, Cmd.none
-    | ClearLoginForm ->
-        { model with
-            username = ""
-            password = "" }, Cmd.none
-    | GetSignedInAs ->
-        model, Cmd.OfAuthorized.either remote.getUsername () RecvSignedInAs Error
-    | RecvSignedInAs username ->
-        { model with signedInAs = username }, onSignIn username
-    | SendSignIn ->
-        model, Cmd.OfAsync.either remote.signIn (model.username, model.password) RecvSignIn Error
-    | RecvSignIn username ->
-        { model with signedInAs = username; signInFailed = Option.isNone username }, onSignIn username
-    | SendSignOut ->
-        model, Cmd.OfAsync.either remote.signOut () (fun () -> RecvSignOut) Error
-    | RecvSignOut ->
-        { model with signedInAs = None; signInFailed = false }, Cmd.none
     | SelectScenario scenarioId ->
         match tryFindScenario scenarioId with
         | Some scenario ->
@@ -170,10 +72,9 @@ let update remote message model =
         | None ->
             model, Cmd.none
 
-    | Error RemoteUnauthorizedException ->
-        { model with error = Some "You have been logged out."; signedInAs = None }, Cmd.none
     | Error exn ->
         { model with error = Some exn.Message }, Cmd.none
+
     | ClearError ->
         { model with error = None }, Cmd.none
 
@@ -422,47 +323,6 @@ let homePage model dispatch =
             text "No demo scenarios are available."
         }
 
-let counterPage model dispatch =
-    Main.Counter()
-        .Decrement(fun _ -> dispatch Decrement)
-        .Increment(fun _ -> dispatch Increment)
-        .Value(model.counter, fun v -> dispatch (SetCounter v))
-        .Elt()
-
-let dataPage model (username: string) dispatch =
-    Main.Data()
-        .Reload(fun _ -> dispatch GetBooks)
-        .Username(username)
-        .SignOut(fun _ -> dispatch SendSignOut)
-        .Rows(cond model.books <| function
-            | None ->
-                Main.EmptyData().Elt()
-            | Some books ->
-                forEach books <| fun book ->
-                    tr {
-                        td { book.title }
-                        td { book.author }
-                        td { book.publishDate.ToString("yyyy-MM-dd") }
-                        td { book.isbn }
-                    })
-        .Elt()
-
-let signInPage model dispatch =
-    Main.SignIn()
-        .Username(model.username, fun s -> dispatch (SetUsername s))
-        .Password(model.password, fun s -> dispatch (SetPassword s))
-        .SignIn(fun _ -> dispatch SendSignIn)
-        .ErrorNotification(
-            cond model.signInFailed <| function
-            | false -> empty()
-            | true ->
-                Main.ErrorNotification()
-                    .HideClass("is-hidden")
-                    .Text("Sign in failed. Use any username and the password \"password\".")
-                    .Elt()
-        )
-        .Elt()
-
 let menuItem (model: Model) (page: Page) (text: string) =
     Main.MenuItem()
         .Active(if model.page = page then "is-active" else "")
@@ -473,18 +333,11 @@ let menuItem (model: Model) (page: Page) (text: string) =
 let view model dispatch =
     Main()
         .Menu(concat {
-            menuItem model Home "Gaia Probe"
-            menuItem model Counter "Counter"
-            menuItem model Data "Download data"
+            menuItem model Probe "Gaia Probe"            
         })
         .Body(
             cond model.page <| function
-            | Home -> homePage model dispatch
-            | Counter -> counterPage model dispatch
-            | Data ->
-                cond model.signedInAs <| function
-                | Some username -> dataPage model username dispatch
-                | None -> signInPage model dispatch
+            | Probe -> homePage model dispatch
         )
         .Error(
             cond model.error <| function
@@ -503,9 +356,7 @@ type MyApp() =
     override _.CssScope = CssScopes.MyApp
 
     override this.Program =
-        let bookService = this.Remote<BookService>()
-        let update = update bookService
-        Program.mkProgram (fun _ -> initModel, Cmd.ofMsg GetSignedInAs) update view
+        Program.mkProgram (fun _ -> initModel, Cmd.none) update view
         |> Program.withRouter router
 #if DEBUG
         |> Program.withHotReload
